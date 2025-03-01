5c3ac574e85a26efc177344a24231127aede12fa
oliveira <oliveiragabriel.dev@gmail.com>
2025-03-01 16:44:39 -0300

    wip

--- bin/main.ml
+++ bin/main.ml
@@ -1,50 +1,109 @@
-let str = "let a = 10"
+let code = "(); abc abc ; 21312"
 
-(* todo: do it better *)
-let is_numeric char = Option.is_some @@ int_of_string_opt char
+type token =
+  | Semicolon
+  | LeftParen
+  | RightParen
+  | Identifier of string
+  | Integer of int
 
-type token_kind =
-  | T_left_paren
-  | T_right_paren
-  | T_equal
-  | T_string of string
-  | T_int of int
+type lexer = { input : string; ch : char option; position : int }
 
-type state = { input : string; rest : string; pos : int }
-        
-and read_number state = (state, T_int 1)
-and read_string state = (state, T_string "")
+let print_opt = function
+  | Some value -> print_endline value
+  | None -> print_endline "None"
 
-and advance state steps =
-  {
-    input = state.input;
-    rest = Str.string_after state.rest steps;
-    pos = state.pos + steps;
-  }
+let print_token = function
+  | Semicolon -> print_endline "Semicolon"
+  | LeftParen -> print_endline "LeftParen"
+  | RightParen -> print_endline "RightParen"
+  | Identifier i -> Printf.printf "Identifier: %s\n" i
+  | Integer i -> Printf.printf "Integer: %d\n" i
 
-and read_char state =
-  let char = Str.string_before state.rest 1 in
+let rec skip_whitespaces lexer = read_while lexer is_whitespace
 
-  (char, advance state 1)
+and seek lexer condition =
+  let rec loop lexer =
+    match lexer.ch with
+    | Some ch when condition ch -> loop (advance lexer)
+    | _ -> lexer
+  in
 
-let rec tokenize state =
-  if state.pos > String.length state.input then state
+  loop lexer
+
+and read_while lexer predicate =
+  let pos_start = lexer.position in
+
+  let lexer = seek lexer predicate in
+
+  let pos_end = lexer.position + 1 in
+
+  let slice = String.sub lexer.input pos_start (pos_end - pos_start) in
+
+  (lexer, slice)
+
+and is_whitespace ch = ch = ' ' || ch = '\n' || ch = '\t'
+
+and is_alpha ch =
+  match ch with 'a' .. 'z' | 'A' .. 'Z' | '_' -> true | _ -> false
+
+and is_identifier ch = is_alpha ch
+and is_number ch = Option.is_some @@ int_of_string_opt @@ Char.escaped ch
+
+and read_identifier lexer =
+  let lexer, slice = read_while lexer is_identifier in
+  (lexer, Identifier slice)
+
+and read_number lexer =
+  let lexer, slice = read_while lexer is_number in
+
+  (* convering might raise an error *)
+  let number = int_of_string slice in
+
+  (lexer, Integer number)
+
+and advance lexer =
+  if lexer.position >= String.length lexer.input - 1 then
+    { lexer with ch = None }
   else
-    let c, state = read_char state in
+    let position = lexer.position + 1 in
+    { lexer with ch = Some (String.get lexer.input position); position }
 
-    let () = print_endline c in
+let next_token lexer =
+  let lexer, _ = skip_whitespaces lexer in
 
-    let state, _ =
-      match c with
-      | "=" -> (state, T_equal)
-      | "(" -> (state, T_left_paren)
-      | ")" -> (state, T_right_paren)
-      | c when is_numeric c -> read_number state
-      | _ -> read_string state
-    in
+  match lexer.ch with
+  | None -> (advance lexer, None)
+  | Some ch ->
+      let lexer, token =
+        match ch with
+        | ';' -> (advance lexer, Semicolon)
+        | '(' -> (advance lexer, LeftParen)
+        | ')' -> (advance lexer, RightParen)
+        | ch when is_identifier ch -> read_identifier lexer
+        | ch when is_number ch -> read_number lexer
+        | ch -> failwith @@ Printf.sprintf "Unexpected character: %c" ch
+      in
 
-    tokenize state
+      (lexer, Some token)
 
-let () = let _ = tokenize { input = str; rest = str; pos = 1 } in
+let init input =
+  if String.length input > 0 then
+    { input; ch = Some (String.get input 0); position = 0 }
+  else { input; ch = None; position = 0 }
 
-         ()
+let rec run_lexer lexer =
+  let rec loop lexer tokens =
+    let lexer, token = next_token lexer in
+
+    match token with
+    | Some token -> loop lexer (token :: tokens)
+    | None -> tokens
+  in
+
+  loop lexer []
+
+let () =
+  let tokens = init code |> run_lexer in
+
+  List.iter print_token @@ List.rev tokens
